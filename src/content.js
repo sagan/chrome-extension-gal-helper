c05eff12f223b2e8c315de0ba12b6a7bbd90a9a5
Jacques De SAGAN <jacquesde@sagan.me>
2015-11-29 14:06:27 +0800

    Add baidupan share code detection and display context menus to search or go to baidupan

--- src/content.js
+++ src/content.js
@@ -1,1 +1,19 @@
 
+document.addEventListener('selectionchange', function() {
+    var selection = window.getSelection();
+    if( selection ) {
+        var msg = {
+            request: 'updateContextMenu',
+            selection: selection.toString(),
+        };
+        if (selection.rangeCount > 0) {
+            var node = selection.getRangeAt(0).startContainer.parentNode;
+            if( node.tagName.toLowerCase() == 'a' )
+                node = node.parentNode;
+            msg.context = node.textContent.trim();
+        }
+        chrome.runtime.sendMessage(msg);
+    }
+});
+
+// window.getSelection().getRangeAt(0).startContainer.parentNode.textContent.trim()
